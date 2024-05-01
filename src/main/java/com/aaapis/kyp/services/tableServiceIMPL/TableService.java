c9f8758c2c459ed960d6acdc58976ac64806496c
Aniruddha <aniruddha.23bcs10156@ms.sst.scaler.com>
2024-05-02 04:56:08 +0530

    Fixes

---
+++ src/main/java/com/aaapis/kyp/services/tableServiceIMPL/TableService.java
@@ -0,0 +1,8 @@
+package com.aaapis.kyp.services.tableServiceIMPL;
+
+import com.aaapis.kyp.services.ITableService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TableService implements ITableService {
+}
