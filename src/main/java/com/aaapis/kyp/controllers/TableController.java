1e0ce2487d94ce92c119831ba4cbbbcdd7412c5b
Attachment Aditya <attachment.aditya@gmail.com>
2024-05-02 04:55:23 +0530

    Update TableController.java

--- src/main/java/com/aaapis/kyp/controllers/TableController.java
+++ src/main/java/com/aaapis/kyp/controllers/TableController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("tables")
+@RequestMapping("/tables")
 public class TableController {
 }
