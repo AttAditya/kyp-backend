b01cc4d27f359179fd1d730ff947585e20175f0d
Attachment Aditya <attachment.aditya@gmail.com>
2024-05-01 21:19:15 +0530

    Update EmployeeRepository.java
    Added repository annotation

--- src/main/java/com/aaapis/kyp/repositories/EmployeeRepository.java
+++ src/main/java/com/aaapis/kyp/repositories/EmployeeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee save(Employee employee);
     Optional<Employee> findById(Long employeeId);
