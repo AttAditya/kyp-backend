70ecef21b3ce8c726d7c6b04b9312d04376689cc
deysandip301 <deysandip301@gmail.com>
2024-05-01 23:16:56 +0530

    updated reservationRepository

--- src/main/java/com/aaapis/kyp/repositories/ReservationRepository.java
+++ src/main/java/com/aaapis/kyp/repositories/ReservationRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    Reservation save(Reservation reservation);
     Optional<Reservation> findById(Long reservationId);
 }
