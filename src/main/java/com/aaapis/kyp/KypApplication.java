ceb64ed3a1e17ddf3a4adafe2721b5ae39b4ee3e
AttAditya <attachment.aditya@gmail.com>
2024-05-01 16:03:01 +0530

    - Created SpringBoot Project

---
+++ src/main/java/com/aaapis/kyp/KypApplication.java
@@ -0,0 +1,13 @@
+package com.aaapis.kyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KypApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaapis/kyp/KypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaapis.kyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
