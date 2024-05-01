cdb84fc1f4619694ab32225f406ccf876bf204b6
Attachment Aditya <attachment.aditya@gmail.com>
2024-05-02 01:31:50 +0530

    Update EmptyGuestListException.java
    code formatting

--- src/main/java/com/aaapis/kyp/exceptions/EmptyGuestListException.java
+++ src/main/java/com/aaapis/kyp/exceptions/EmptyGuestListException.java
@@ -3,6 +3,6 @@ package com.aaapis.kyp.exceptions;
 public class EmptyGuestListException extends RuntimeException {
     public EmptyGuestListException (String message)
     {
-        super (message);
+        super(message);
     }
 }
