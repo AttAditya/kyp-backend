ffcc954a0a6dc99ccc420467989650dd5d527dc1
indrajeetyadav <indrajeetyadav993648@gmail.com>
2024-05-02 00:10:48 +0530

    - Added Repositories for Reservation, Customer, Employee , Reservation, table and Location

--- src/main/java/com/aaapis/kyp/repositories/LocationRepository.java
+++ src/main/java/com/aaapis/kyp/repositories/LocationRepository.java
@@ -2,9 +2,11 @@ package com.aaapis.kyp.repositories;
 
 import com.aaapis.kyp.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location,Long> {
     @Override
     Optional<Location> findById(Long locationId);
--- src/main/java/com/aaapis/kyp/repositories/TableRepository.java
+++ src/main/java/com/aaapis/kyp/repositories/TableRepository.java
@@ -2,9 +2,11 @@ package com.aaapis.kyp.repositories;
 
 import com.aaapis.kyp.models.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TableRepository extends JpaRepository<Table,Long> {
     @Override
     Optional<Table> findById(Long tableId);
